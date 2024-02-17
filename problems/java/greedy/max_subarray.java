bfea1b7e132bb4a480b43f6fbcaab2633548e811
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-02-17 17:37:17 -0500

    adding printing the resulting subarray

--- problems/java/greedy/max_subarray.java
+++ problems/java/greedy/max_subarray.java
@@ -14,3 +14,29 @@ class Solution {
         return max;
     }
 }
+
+// VERSION with printing the resulting optimal subarray
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int max = nums[0];
+//         int curSum = nums[0];
+//         int start = 0;
+//         int end = 0;
+
+//         for(int i=1; i<nums.length; i++){
+//             if(curSum < 0){
+//                 curSum = 0;
+//                 start = i;
+//             }
+//             curSum += nums[i];
+//             max = Math.max(max, curSum);
+//             end = i;
+//         }
+//         for(int i = start; i<=end; i++){
+//             System.out.print(nums[i]+" ");
+//         }
+
+//         return max;
+//     }
+// }
