5f4a0e6c3ba3766cadf8134afaa62f951fad5075
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-05-30 16:04:57 -0400

    optimization that exploits that we sort the array and uses two pointers

--- problems/java/two_pointers/treeSum.java
+++ problems/java/two_pointers/treeSum.java
@@ -5,50 +5,76 @@ class Solution
         List<List<Integer>> res = new ArrayList<>();
         int l,r;
         Arrays.sort(nums);
-        // for (int i=0; i< nums.length; i++)
-            // System.out.println(nums[i]);
         for (int i=0; i<nums.length-2; i++)
         {
             if ( i!=0 && nums[i]==nums[i-1])
                 continue;
-            List<List<Integer>> curRes = twoSum(i+1,nums, -nums[i]);
-            // System.out.println(nums[i]);
-            for(List<Integer> li : curRes)
-            {
-                li.add(nums[i]);
-                res.add(li);
-            }
+            twoSumSorted(i+1,nums, -nums[i],res);
         }
 
         return res;
     }
 
-    public List<List<Integer>> twoSum(int l, int[] nums, int target)
+    void twoSumSorted(int l, int[] nums, int target, List<List<Integer>> res)
     {
-        // System.out.println(target);
-        // key: target 
-        // value: num
-        int initial = l;
-        HashMap<Integer,Integer> hash = new HashMap<>();
-        List<List<Integer>> res = new ArrayList<>();
+
+        int r = nums.length-1, sum;
         // O(n)
-        while (l<nums.length)
+        while (l<r)
         {
-            if( hash.containsKey(nums[l]))
+            sum = nums[l]+nums[r];
+
+            if(sum<target)
             {
-                // System.out.println("found: " +nums[l]+" "+hash.get(nums[l]));
-                res.add(new ArrayList<>(Arrays.asList(nums[l],hash.get(nums[l]))));
-                while(l<nums.length-1 && nums[l]==nums[l+1])
+                do
+                {
                     l++;
+                }
+                while(l<r && nums[l] == nums[l-1]);
             }
-            else
+            else if (sum>target)
             {
-                hash.put(target-nums[l],nums[l]);
+                do
+                {
+                    r--;
+                }
+                while(l<r && nums[r] == nums[r+1]);
             }
-            l++;
+            else /*sum == target*/
+            {
+                res.add(new ArrayList<>(Arrays.asList(-target, nums[l],nums[r])));
+                do
+                {
+                    l++;
+                }
+                while(l<r && nums[l] == nums[l-1]);
+            }
         }
 
-        return res;
-
     }
+
+    // public List<List<Integer>> twoSum(int l, int[] nums, int target, List<List<Integer>> res)
+    // {
+
+    //     int initial = l;
+    //     HashMap<Integer,Integer> hash = new HashMap<>();
+
+    //     // O(n)
+    //     while (l<nums.length)
+    //     {
+    //         if( hash.containsKey(nums[l]))
+    //         {
+    //             res.add(new ArrayList<>(Arrays.asList(-target, nums[l],hash.get(nums[l]))));
+    //             while(l<nums.length-1 && nums[l]==nums[l+1])
+    //                 l++;
+    //         }
+    //         else
+    //         {
+    //             hash.put(target-nums[l],nums[l]);
+    //         }
+    //         l++;
+    //     }
+
+    //     return res;
+    // }
 }
