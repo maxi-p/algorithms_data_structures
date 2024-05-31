151622e03da2cf76d6d1081321eff2e8ad73fe24
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-05-31 10:58:13 -0400

    optimization. spliting the lists in 2 and reversing the second one and then mergin them intead of using the Linked list from previous solution.

--- problems/java/linked_list/reorder_list.java
+++ problems/java/linked_list/reorder_list.java
@@ -12,43 +12,92 @@ class Solution
 {
     public void reorderList(ListNode head) 
     {
-        HashMap<Integer,ListNode> hash = new HashMap<>();
 
-        int n=0;
-        while (head != null)
+        // find the half
+        ListNode    slow        = head;
+        ListNode    prevSlow    = null;
+        ListNode    fast        = head;
+
+        while (fast!=null && fast.next!=null)
         {
-            hash.put(n++, head);
-            head = head.next;
+            fast        = fast.next.next;
+            prevSlow    = slow;
+            slow        = slow.next;
+        }
+        if(prevSlow == null)
+            return;
+        prevSlow.next = null;
+        // slow now has the beginning node of middle
+
+        // flip the second half (flow slow)
+        ListNode prev = null;
+        while(slow !=null)
+        {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
         }
 
-        int a = 0;
-        int b = n-1;
-        int i = 0;
-        while ( i < n )
+        // merge the two halfs ( head and prev)
+        int i=0;
+        while(head != null)
         {
-            if(i%2 == 0)
+            if (i%2==0)
             {
-                if (i==n-1)
-                {
-                    hash.get(a).next = null;
-                }
-                else
-                {
-                    hash.get(a++).next = hash.get(b);
-                }
+                ListNode temp = head.next;
+                head.next = prev;
+                head = temp;
             }
-            if(i%2 == 1)
+            else /* i%2==1 */
             {
-                if (i==n-1)
-                {
-                    hash.get(b).next = null;
-                }
-                else
-                {
-                    hash.get(b--).next = hash.get(a);
-                }
+                ListNode temp = prev.next;
+                prev.next = head;
+                prev = temp;
             }
             i++;
         }
     }
+
+    // public void reorderListArrayListSolution(ListNode head) 
+    // {
+    //     HashMap<Integer,ListNode> hash = new HashMap<>();
+
+    //     int n=0;
+    //     while (head != null)
+    //     {
+    //         hash.put(n++, head);
+    //         head = head.next;
+    //     }
+
+    //     int a = 0;
+    //     int b = n-1;
+    //     int i = 0;
+    //     while ( i < n )
+    //     {
+    //         if(i%2 == 0)
+    //         {
+    //             if (i==n-1)
+    //             {
+    //                 hash.get(a).next = null;
+    //             }
+    //             else
+    //             {
+    //                 hash.get(a++).next = hash.get(b);
+    //             }
+    //         }
+    //         if(i%2 == 1)
+    //         {
+    //             if (i==n-1)
+    //             {
+    //                 hash.get(b).next = null;
+    //             }
+    //             else
+    //             {
+    //                 hash.get(b--).next = hash.get(a);
+    //             }
+    //         }
+    //         i++;
+    //     }
+    // }
 }
