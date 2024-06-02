51a1c8300d50b78fd20d8a432a3a4c8117d58383
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-06-01 23:59:52 -0400

    xor optimal solution

--- problems/java/bitwise_manipulations/missing_number.java
+++ problems/java/bitwise_manipulations/missing_number.java
@@ -2,19 +2,35 @@ class Solution
 {
     public int missingNumber(int[] nums) 
     {
-        int len = nums.length;
-        int res=0;
+        if(nums.length == 1)
+            return 1-nums[0];
 
-        for(int i=0; i<=len; i++)
+        int res = nums[0];
+        for(int i=1; i<nums.length; i++)
         {
-            res+=i;
+            res = res ^ nums[i];
+            res = res ^ i;
         }
-
-        for(int i=0; i<len; i++)
-        {
-            res-=nums[i];
-        }
+        res = res ^ nums.length;
 
         return res;
     }
+
+    // public int missingNumberNaive(int[] nums) 
+    // {
+    //     int len = nums.length;
+    //     int res = 0;
+
+    //     for(int i=0; i<=len; i++)
+    //     {
+    //         res += i;
+    //     }
+
+    //     for(int i=0; i<len; i++)
+    //     {
+    //         res -= nums[i];
+    //     }
+
+    //     return res;
+    // }
 }
