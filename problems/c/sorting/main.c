d7f451b63c9496580d7253f11bc262702857b92c
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-07-28 08:39:40 -0400

    Implementing selection sort in C

---
+++ problems/c/sorting/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "selectionSort.h"
+
+int main(int argc, char **argv)
+{
+  FILE *fp;
+  char buffer[16], c;
+  int arrayLength;
+  int temp;
+  int *array;
+
+  for (int i = 1; i < argc; i++)
+  {
+    fp = fopen(*(argv + i), "r");
+
+    fscanf(fp, "%d\n", &arrayLength);
+    array = (int *)malloc(arrayLength * sizeof(int));
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+      fscanf(fp,"%s, ", buffer);
+      sscanf(buffer, "%d", array+i);
+    }
+
+    printf("\nBefore the sort:\n---------------\n");
+    for (int i=0; i< arrayLength; i++)
+    {
+      printf("%d\n", array[i]);
+    }
+
+    selectionSort(array, arrayLength);
+    printf("\nAfter the sort:\n--------------\n");
+    for (int i=0; i< arrayLength; i++)
+    {
+      printf("%d\n", array[i]);
+    }
+
+    free(array);
+    fclose(fp);
+  }
+  return 0;
+}
---
+++ problems/c/sorting/selectionSort.h
@@ -0,0 +1,5 @@
+
+#pragma once
+#include <math.h>
+
+void selectionSort(int *arr, int length);
